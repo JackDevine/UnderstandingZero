12cf4d36725dd837ed1febb8f5ad6f9e7cf7c727
Jack Devine <8610352+JackDevine@users.noreply.github.com>
2024-02-18 06:44:24 +0000

    automatic backup

---
+++ new/Surprising invention.R
@@ -0,0 +1,7 @@
+### A Pluto.jl notebook ###
+# v0.19.28
+# Welcome to your R reactive notebook.# ╔═╡ 17a16ee6-ce29-11ee-13e2-45ef8693cf8b
+
+
+# ╔═╡ Cell order:
+# ╠═17a16ee6-ce29-11ee-13e2-45ef8693cf8b
