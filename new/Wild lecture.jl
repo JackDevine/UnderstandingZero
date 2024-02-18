02589167400c73721c4c148295344f6cbf804e22
Jack Devine <8610352+JackDevine@users.noreply.github.com>
2024-02-18 06:30:23 +0000

    automatic backup

--- new/Wild lecture.jl
+++ new/Wild lecture.jl
@@ -4,6 +4,9 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 35e504df-05d0-46b9-ab18-803c93ac5b17
+using Plots
+
 # ╔═╡ ad9da0a0-ce0f-11ee-1c3d-e39d4c5e2380
 begin
 	randn(50, 50)
@@ -12,3 +15,4 @@ end
 
 # ╔═╡ Cell order:
 # ╠═ad9da0a0-ce0f-11ee-1c3d-e39d4c5e2380
+# ╠═35e504df-05d0-46b9-ab18-803c93ac5b17
