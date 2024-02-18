c9d78eb65648474ccfaa1b1931c58d8b6a99f700
Jack Devine <8610352+JackDevine@users.noreply.github.com>
2024-02-18 06:46:34 +0000

    automatic backup

---
+++ new/Stunning creation.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.28
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 5bf7c252-ce29-11ee-1a27-f501fc9476e5
+sum(1/n^2 for n in 1:1000)
+
+# ╔═╡ Cell order:
+# ╠═5bf7c252-ce29-11ee-1a27-f501fc9476e5
