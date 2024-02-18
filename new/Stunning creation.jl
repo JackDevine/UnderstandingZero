3b79b6d42464259e61bd81a8059547a95dbd0f44
Jack Devine <8610352+JackDevine@users.noreply.github.com>
2024-02-18 06:47:12 +0000

    automatic backup

--- new/Stunning creation.jl
+++ new/Stunning creation.jl
@@ -4,8 +4,12 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 360d28ef-3925-43d0-87b6-a29c3aae03af
+
+
 # ╔═╡ 5bf7c252-ce29-11ee-1a27-f501fc9476e5
-sum(1/i^2 for i in 1:1000)
+sum(1/i^2 for i in 1:n) - π^2/6
 
 # ╔═╡ Cell order:
+# ╠═360d28ef-3925-43d0-87b6-a29c3aae03af
 # ╠═5bf7c252-ce29-11ee-1a27-f501fc9476e5
