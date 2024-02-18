ef2f268d8cff7d11c5b1d5663bcd65d0d3ecce8d
Jack Devine <8610352+JackDevine@users.noreply.github.com>
2024-02-18 06:45:46 +0000

    automatic backup

---
+++ new/Remarkable blueprint.R
@@ -0,0 +1,7 @@
+### A Pluto.jl notebook ###
+# v0.19.28
+# Welcome to your R reactive notebook.# ╔═╡ 489c8a4e-ce29-11ee-2f22-ed494c706834
+
+
+# ╔═╡ Cell order:
+# ╠═489c8a4e-ce29-11ee-2f22-ed494c706834
