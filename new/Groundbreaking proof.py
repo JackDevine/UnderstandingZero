6cab2863f4f4f226461a3c41ff2e9bfcbebbaac9
Jack Devine <8610352+JackDevine@users.noreply.github.com>
2024-02-18 03:42:37 +0000

    automatic backup

---
+++ new/Groundbreaking proof.py
@@ -0,0 +1,7 @@
+### A Pluto.jl notebook ###
+# v0.19.28
+# Welcome to your Python reactive notebook.# ╔═╡ b1a65c82-ce0f-11ee-3083-d3eaad37f2e6
+2+2
+
+# ╔═╡ Cell order:
+# ╠═b1a65c82-ce0f-11ee-3083-d3eaad37f2e6
---
+++ new/Wild lecture.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.28
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ad9da0a0-ce0f-11ee-1c3d-e39d4c5e2380
+randn(50, 50)
+
+# ╔═╡ Cell order:
+# ╠═ad9da0a0-ce0f-11ee-1c3d-e39d4c5e2380
